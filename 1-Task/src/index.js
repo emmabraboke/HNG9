4a92d40dcdddbcec761a285b11f9c6ba0680a2ad
emmabraboke <emmabraboke@gmail.com>
2022-10-28 08:31:20 -0700

    add all files

--- 1-Task/src/index.js
+++ 1-Task/src/index.js
@@ -1,9 +1,13 @@
 import express from "express"
 import {port} from "../config/index.js"
 import userRoute from "./routes/user.route.js"
+import cor from "cors"
 
 const app = express()
 
+app.use(cor({
+    origin: "*"
+}))
 app.use("/api/v1", userRoute)
 
 const Port = port || 5000
