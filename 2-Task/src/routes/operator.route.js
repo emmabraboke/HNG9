8bf4c955b09989d9e6581813dee7d969ec20ae4a
emmabraboke <emmabraboke@gmail.com>
2022-12-08 17:55:05 +0100

    add files

--- 2-Task/src/routes/operator.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createOperation } from "../controller/operation.controller.js";
-
-const operationRoute = Router()
-
-operationRoute.route("/operation").post(createOperation)
-
-export default operationRoute
--- 2-Task/src/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import { getUser } from "../controller/user.controller.js";
-import { Router } from "express";
-
-const userRoute = Router()
-
-userRoute.route("/user").get(getUser)
-
-export default userRoute
